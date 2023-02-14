2fafc0253bfeea5a5a5f648aea7570a51edab582
Chris Barclay <chris.barclay1@gmail.com>
2023-02-14 16:46:14 +0000

    Set up dummy data in console.

--- console.py
+++ console.py
@@ -1,5 +1,15 @@
 import pdb
 from models.album import Album
 from models.artist import Artist
-import repositories.album_test as album_test
-import repositories.artist_test as artist_test
+import repositories.album_repository as album_repository
+import repositories.artist_repository as artist_repository
+
+artist_1 = Artist("Talking Heads")
+album_1 = Album("Speaking in Tongues", "New Wave", artist_1)
+album_2 = Album("Little Creatures", "New Wave", artist_1)
+
+artist_2 = Artist("Aphex Twin")
+album_3 = Album("Syro", "Electronica", artist_2)
+
+
+pdb.set_trace()
